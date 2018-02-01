0a669f9250d5f6993d716b3473c8a63d134122bc
Matt Scheetz <mfscheetz@gmail.com>
2018-01-31 19:50:12 -0500

    Exchange enum

---
+++ src/app/classes/cryptoBits/exchange.ts
@@ -0,0 +1,21 @@
+/**
+ * Enum of exchanges
+ */
+export enum Exchange {
+    Altcoin,
+    Binance,
+    Bitfinex,
+    Bittrex,
+    Coinbase,
+    Coinexchange,
+    Cointopia,
+    Coss,
+    GDAX,
+    Gemini,
+    Hitbtc,
+    Kraken,
+    Kucoin,
+    Poloniex,
+    StocksExchange,
+    Upcoin
+}
