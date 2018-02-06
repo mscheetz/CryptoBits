9162988bb318ffb5f42e95158b138b199d9cadcd
Matt Scheetz <mfscheetz@gmail.com>
2018-02-05 20:01:05 -0500

    99CryptoCoin api and return type

---
+++ src/app/apiAccess/ninetyNineCryptoApi.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'application/x-www-form-urlencoded';
+import { RequestOptions } from '@angular/http/src/base_request_options';
+import { RestApi } from './restApi';
+import { HttpMethod } from "blocking-proxy/built/lib/webdriver_commands";
+import { isNullOrUndefined } from 'util';
+import { CryptoJS } from "crypto-js";
+
+export class NinetyNineCryptoApi {
+  
+  private apiBase = 'https://api.99cryptocoin.com';
+  private apiComplete = '';
+  private headers;
+  private options;
+  private restApi;
+  data: any = {};
+
+  constructor() {
+    this.restApi = new RestApi();
+
+    console.log('99 Crypto Coin APIs');
+  }
+
+  /**
+   *  Get coin name/symbol.
+   */
+  public async getCoins(): Promise<Coin[]>{
+    let url = this.apiBase + "/v1/coins";
+
+    let results: any = await this.nnccRequest(
+      'GET',
+      url,
+      false
+    );
+
+    return results.result;
+  }
+
+  /**
+   * Request a 99 Crypto Coin api
+   * 
+   * @param httpMethod      Http Method type
+   * @param url             Url to access
+   * @param secured         Is the request secured?
+   * @param parameters      Optional query parameters
+   */
+  private async nnccRequest(
+    httpMethod: string,
+    url: string,
+    secured: boolean,
+    ...parameters: [ string, any ][]): Promise<any>{
+      
+      let uri: URL = new URL(
+        url,
+        this.apiBase
+      );
+
+      for(let parameter of parameters){
+        if(isNullOrUndefined(parameter[1])){
+          continue;
+        }
+        uri.searchParams.append(parameter[0], parameter[1].toString());
+      }
+
+      return await this.restApi.apiRequest(
+        httpMethod,
+        uri.href
+      );
+
+  }
+}
---
+++ src/app/classes/99Crypto/coin.ts
@@ -0,0 +1,38 @@
+/**
+ * Represents a single coin from 99CryptoCoins
+ */
+export class Coin {
+    private _id: string;
+    private _name: string;
+    private _symbol: string;
+
+    constructor (json: any) {
+        this._id = json.id;
+        this._name = json.name;
+        this._symbol = json.symbol;
+    }
+
+    get id(): string {
+        return this._id;
+    }
+
+    set id(value: string) {
+        this._id = value;
+    }
+    
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+    
+    get symbol(): string {
+        return this._symbol;
+    }
+
+    set symbol(value: string) {
+        this._symbol = value;
+    }
+}
