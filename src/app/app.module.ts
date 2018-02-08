48129faf4644de56c448a09deb3398bc038f1198
Matt Scheetz <mfscheetz@gmail.com>
2018-02-07 20:55:01 -0500

    Updates to base ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './ui/portfolio/portfolio.component';
+import { ApiInfoComponent } from './ui/apiInformation/apiInfo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    ApiInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
